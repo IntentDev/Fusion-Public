0af958057be312b0beb63a7cf6d1e78fa0b641b1
keithlostracco <keith@IntentDev.io>
2019-08-12 08:11:09 -0700

    Added Touch in/out sync method

--- project/source/modules/SyncExt.py
+++ project/source/modules/SyncExt.py
@@ -8,37 +8,54 @@ class SyncExt:
 		self.syncIn = ownerComp.op('syncin')
 		self.shdmemOut = ownerComp.op('sharedmemoutSync')
 		self.shdmemIn = ownerComp.op('sharedmeminSync')
+		self.touchOut = ownerComp.op('touchoutSync')
+		self.touchIn = ownerComp.op('touchinSync')
 		self.switchSync = ownerComp.op('switchSync')
 		self.syncSources = ownerComp.op('syncSources')
 		self.selCrossDataComp1 = self.syncSources.op('selCrossDataComp1')
 		self.selCrossDataComp2 = self.syncSources.op('selCrossDataComp2')
 
 	def SyncOutActive(self, state):
-
 		if self.Syncmode == 'SYNC_CHOP':
 			self.syncOut.par.active = state
 			self.syncOut.bypass = not state
-			#self.shdmemOut.par.active = False
-		else:
+			self.shdmemOut.par.active = False
+			self.touchOut.par.active = False
+
+		elif self.Syncmode == 'SHARED_MEM':
 			self.syncOut.par.active = False
 			self.syncOut.bypass = True
-			#self.shdmemOut.par.active = True
+			self.shdmemOut.par.active = state
+			self.touchOut.par.active = False
+
+		elif self.Syncmode == 'TOUCH_IN_OUT':
+			self.syncOut.par.active = False
+			self.syncOut.bypass = True
+			self.shdmemOut.par.active = False
+			self.touchOut.par.active = state			
 
 	def SyncInActive(self, state):
-
 		if self.Syncmode == 'SYNC_CHOP':		
 			self.syncIn.par.active = state
 			self.syncIn.bypass = not state
 			self.shdmemIn.par.active = False
-		else:	
+			self.touchIn.par.active = False
+
+		elif self.Syncmode == 'SHARED_MEM':
 			self.syncIn.par.active = False
 			self.syncIn.bypass = True
 			self.shdmemIn.par.active = state
+			self.touchIn.par.active = False
+
+		elif self.Syncmode == 'TOUCH_IN_OUT':
+			self.syncIn.par.active = False
+			self.syncIn.bypass = True
+			self.shdmemIn.par.active = False
+			self.touchIn.par.active = state			
 		
 		self.switchSync.par.index = int(state)
 
 	def SetCrossComps(self, comp1, comp2):
-		
 		index1 = comp1.op('index')
 		self.selCrossDataComp1.par.chop = index1.path
 		self.selCrossDataComp1.par.renameto = index1.path
